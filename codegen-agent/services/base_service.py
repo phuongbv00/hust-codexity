6dfc10385cf889dc5dfbc482490ffb4f6cb0c1ba
BN2002 <doanhcms@gmail.com>
2025-02-27 23:01:02 +0700

    ChatGPT Service

--- codegen-agent/services/base_service.py
+++ codegen-agent/services/base_service.py
@@ -1,12 +1,88 @@
 from models.generate_code import GenerateCodeRequest
+from models.generate_question_prompt import GenerateQuestionPromptRequest
 from abc import abstractmethod
-
+from models.vulnerability import Vulnerability
 class BaseLLMService:
     @abstractmethod
     def generate_code(self, request: GenerateCodeRequest) -> str:
         return
     
     @abstractmethod
-    def generate_programming_question_prompt(self, request: GenerateCodeRequest) -> str:
+    def generate_programming_question_prompt(self, request: GenerateQuestionPromptRequest) -> str:
         return
+    
+    def get_normal_prompt(self, input: str) -> list:
+        return [
+            {
+                "role": "system", 
+                "content": "Bạn là lập trình viên xuất sắc có thể giúp người dùng tạo ra các đoạn mã"
+            },
+            {
+                "role": "user", 
+                "content": "Tôi sẽ đưa ra ý tưởng, bài toán hoặc một chương trình chưa hoàn thiện.Tôi muốn bạn hoàn thành chương trình đó cho tôi.Chỉ đưa ra mã nguồn của chương trình hoàn chỉnh(không phải chỉ đoạn code bạn viết thêm) ở dạng text, không cần giải thích gì thêm, không cần đưa vào block code markdown"
+            },
+            {
+                "role": "assistant",
+                "content": "Chắc chắn rồi.Bạn hãy đưa ra ý tưởng hoặc đoạn code cần tôi hoàn thiện.Tôi sẽ giúp bạn hoàn thiện chương trình một cách hoàn chỉnh"
+            },
+            {
+                "role": "user",
+                "content": input
+            }
+        ]
+    
+    def get_repair_vulnerability_prompt(self, input, vulnerabilities: list[Vulnerability]) -> list[map]:
+        vuln_info = "Danh sách lỗ hổng: \n"
+        for vuln in vulnerabilities:
+            vuln_info += f"- {vuln.cwe}: {vuln.message}\n"
+            if vuln.location:
+                line_info = f" (Dòng {vuln.location.line})" if hasattr(vuln.location, "line") else ""
+                vuln_info += line_info
+
+        return [
+            {
+                "role": "system", 
+                "content": "Bạn là lập trình viên xuất sắc có thể giúp người dùng tạo ra các đoạn mã"
+            },
+            {
+                "role": "user", 
+                "content": "Tôi sẽ đưa cho bạn một đoạn mã chứa lỗ hổng bảo mật.Sau đó tôi sẽ đưa cho bạn danh sách các lỗ hổng được các công cụ Static Application Security Testing phát hiện ra.Bạn hãy dựa vào danh sách đó để sửa lỗi cho tôi.Bạn chỉ cần đưa ra mã nguồn của chương trình ở dạng text, không cần viết gì thêm, không cần đưa vào block code markdown"
+            },
+            {
+                "role": "assistant", 
+                "content": "Chắc chắn rồi, tôi có thể giúp bạn phân tích mã nguồn và kết quả từ công cụ SAST để xác định và khắc phục các vấn đề. Vui lòng cung cấp mã nguồn và kết quả từ công cụ SAST, và tôi sẽ cố gắng hết sức để hỗ trợ bạn. Tôi sẽ chỉ đưa ra mã nguồn được sửa chữa và không giải thích gì thêm."
+            },
+            {
+                "role": "user", 
+                "content": input
+            },
+            {
+                "role": "assistant", 
+                "content": "Tôi đã thấy mã nguồn bạn cung cấp. Để hỗ trợ bạn tốt hơn, vui lòng cung cấp kết quả từ các công cụ SAST và chỉ rõ các vấn đề hoặc lỗi mà bạn đang gặp phải. Điều này sẽ giúp tôi hiểu rõ vấn đề và đưa ra cách sửa mã chính xác hơn."
+            },
+            {
+                "role": "user", 
+                "content": vuln_info
+            }
+        ]
+    
+    def get_programming_question_prompt(self, input: str) -> list:
+        return [
+            {
+                "role": "system",
+                "content": "Bạn là một kỹ sư phần mềm xuất sắc có thể giúp người dùng tạo ra các câu hỏi lập trình từ đoạn mã có sẵn hoặc câu trả lời."
+            },
+            {
+                "role": "user",
+                "content": "Tôi sẽ cung cấp cho bạn một đoạn mã hoặc câu trả lời. Bạn hãy sử dụng ngữ cảnh này để tạo ra câu hỏi lập trình liên quan đến đoạn mã hoặc câu trả lời đó."
+            },
+            {
+                "role": "assistant",
+                "content": "Chắc chắn rồi. Vui lòng cung cấp ngữ cảnh chứa mã hoặc câu trả lời để tôi có thể tạo ra câu hỏi lập trình liên quan đến ngữ cảnh đó cho bạn cho bạn."
+            },
+            {
+                "role": "user",
+                "content": input
+            }
+        ]
     
--- codegen-agent/services/chatgpt_service.py
+++ codegen-agent/services/chatgpt_service.py
@@ -1,6 +1,7 @@
 from services.base_service import BaseLLMService
 from openai import OpenAI
-from openai.types import Completion
+from models.generate_code import GenerateCodeResponse
+from models.generate_question_prompt import GenerateQuestionPromptResponse
 from config.config import Settings
 class ChatGPTService(BaseLLMService):
     client: OpenAI
@@ -11,15 +12,39 @@ class ChatGPTService(BaseLLMService):
         return 
 
     def generate_code(self, request):
-        response = self.client.completions.create(
-            model=self.settings.open_ai_model,
+        # Thêm thông tin về các lỗ hổng bảo mật nếu có
+        if request.vulnerabilities and len(request.vulnerabilities) > 0:
+            messages = self.get_repair_vulnerability_prompt(request.prompt, request.vulnerabilities)
+        else:
+            # Tạo messages cho API request
+            messages = self.get_normal_prompt(request.prompt)
+        
+        # Gọi đến OpenAI 
+        print(messages)
+        response = self.client.chat.completions.create(
+            model=self.settings.openai_model,
             temperature=request.temperature,
             max_tokens=request.max_tokens,
-            messages=[
-               
-            ]
+            messages=messages
         )
-        return ""
+        
+        # Trích xuất code từ response
+        generated_code = response.choices[0].message.content
+        
+        # Trả về code trong response format
+        return GenerateCodeResponse(code=generated_code)
 
     def generate_programming_question_prompt(self, request): 
-        return ""
+         
+        response = self.client.chat.completions.create(
+            model=self.settings.openai_model,
+            temperature=request.temperature,
+            max_tokens=request.max_tokens,
+            messages=self.get_programming_question_prompt(request.context)
+        )
+        
+        # Trích xuất code từ response
+        prompt = response.choices[0].message.content
+        
+        # Trả về code trong response format
+        return GenerateQuestionPromptResponse(prompt=prompt)
