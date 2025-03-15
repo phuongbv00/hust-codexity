fe39c234ae98bae0fe971f0e70a014c89d70eff9
DinhVu3671 <vuthptnhoquana@gmail.com>
2025-03-16 00:41:46 +0700

    update

--- excuteItelration.py
+++ excuteItelration.py
@@ -2,7 +2,7 @@ import os
 import requests
 import time
 
-BASE_DIR = "Data"
+BASE_DIR = "data"
 URL_ITELRATION = "http://127.0.0.1:8083/iteration-repair"
 
 def codexity():
@@ -12,36 +12,35 @@ def codexity():
     codeNonVul = 0
     codeVul = 0
 
-    for folder in range(1, 91):  # Duyệt từ 1 -> 90
-        folder_path = os.path.join(BASE_DIR, str(folder) + ".c")  # Đường dẫn đến thư mục con
+    # for folder in range(1, 91):  # Duyệt từ 1 -> 90
+    folder_path = os.path.join(BASE_DIR)  # Đường dẫn đến thư mục con
 
-        if not os.path.exists(folder_path):
-            print(f"Warning: {folder_path} not found!")
-            continue  # Nếu thư mục không tồn tại, bỏ qua
+    if not os.path.exists(folder_path):
+        print(f"Warning: {folder_path} not found!")
 
-        # Lặp qua tất cả file .c trong thư mục con
-        i = 1
-        for file_name in os.listdir(folder_path):
-            try:
-                if file_name.endswith(".c"):  # Chỉ xử lý file .c
-                    file_path = os.path.join(folder_path, file_name)
-                    with open(file_path, "r", encoding="utf-8") as f:
-                        code_content = f.read()
-                    payload = {
-                        "prompt" : code_content,  # Tên file
-                        "max_iterations" : 10  # Nội dung code
-                    }
-                    codeProcess += 1
-                    response = requests.post(f"{URL_ITELRATION}", json=payload)
-                    # response.raise_for_status()
-                    dataRes = response.json()
-                    if dataRes["vulnerabilities"]:
-                        codeVul += 1
-                    else:
-                        codeNonVul += 1
-            except Exception as e:
-                print("Error: ", e)
-                continue
+    # Lặp qua tất cả file .c trong thư mục con
+    i = 1
+    for file_name in os.listdir(folder_path):
+        try:
+            if file_name.endswith(".c"):  # Chỉ xử lý file .c
+                file_path = os.path.join(folder_path, file_name)
+                with open(file_path, "r", encoding="utf-8") as f:
+                    code_content = f.read()
+                payload = {
+                    "prompt" : code_content,  # Tên file
+                    "max_iterations" : 10  # Nội dung code
+                }
+                codeProcess += 1
+                response = requests.post(f"{URL_ITELRATION}", json=payload)
+                # response.raise_for_status()
+                dataRes = response.json()
+                if dataRes["vulnerabilities"]:
+                    codeVul += 1
+                else:
+                    codeNonVul += 1
+        except Exception as e:
+            print("Error: ", e)
+            continue
     end_time = time.time()
     result = {
         "codeProcess" : codeProcess,
