97cce5ff1ad56116de07e2b7246b52d58fb54c15
DinhVu3671 <vuthptnhoquana@gmail.com>
2025-03-22 21:57:40 +0700

    update itelration

--- executeIteration.py
+++ executeIteration.py
@@ -2,7 +2,7 @@ import os
 import requests
 import time
 
-BASE_DIR = "data"
+BASE_DIR = "data_v2"
 URL_ITELRATION = "http://127.0.0.1:8083/iteration-repair"
 
 def codexity():
@@ -12,38 +12,39 @@ def codexity():
     codeNonVul = 0
     codeVul = 0
 
-    # for folder in range(1, 91):  # Duyệt từ 1 -> 90
-    folder_path = os.path.join(BASE_DIR)  # Đường dẫn đến thư mục con
+    for folder in range(1, 91):  # Duyệt từ 1 -> 90
+        print("Folder: ", folder)
+        folder_path = os.path.join(BASE_DIR, str(folder) + ".c")  # Đường dẫn đến thư mục con
 
-    if not os.path.exists(folder_path):
-        print(f"Warning: {folder_path} not found!")
+        if not os.path.exists(folder_path):
+            print(f"Warning: {folder_path} not found!")
 
-    # Lặp qua tất cả file .c trong thư mục con
-    i =0
-    for file_name in os.listdir(folder_path):
-        try:
-            if file_name.endswith(".c"):  # Chỉ xử lý file .c
-                file_path = os.path.join(folder_path, file_name)
-                with open(file_path, "r", encoding="utf-8") as f:
-                    code_content = f.read()
-                payload = {
-                    "prompt" : code_content,  # Tên file
-                    "max_iterations" : 10  # Nội dung code
-                }
-                codeProcess += 1
-                response = requests.post(f"{URL_ITELRATION}", json=payload)
-                # response.raise_for_status()
-                dataRes = response.json()
-                if dataRes["vulnerabilities"]:
-                    codeVul += 1
-                else:
-                    codeNonVul += 1
+        # Lặp qua tất cả file .c trong thư mục con
+        i = 0
+        for file_name in os.listdir(folder_path):
+            try:
+                if file_name.endswith(".c"):  # Chỉ xử lý file .c
+                    file_path = os.path.join(folder_path, file_name)
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        code_content = f.read()
+                    payload = {
+                        "prompt" : code_content,  # Tên file
+                        "max_iterations" : 10  # Nội dung code
+                    }
+                    codeProcess += 1
+                    response = requests.post(f"{URL_ITELRATION}", json=payload)
+                    # response.raise_for_status()
+                    dataRes = response.json()
+                    if dataRes["vulnerabilities"]:
+                        codeVul += 1
+                    else:
+                        codeNonVul += 1
 
-                i += 1
-                print("Process: ",i)
-        except Exception as e:
-            print("Error: ", e)
-            continue
+                    i += 1
+                    print("Process: ",i)
+            except Exception as e:
+                print("Error: ", e)
+                continue
     end_time = time.time()
     result = {
         "codeProcess" : codeProcess,
@@ -57,7 +58,7 @@ def codexity():
 
 if __name__ == "__main__":
     response =  codexity()
-    print("Số lượng code ban đầu: ", 90)
+    print("Số lượng code ban đầu: ", 990)
     print("Số lượng code được xử lý chạy qua bộ lọc lỗ hổng bảo mật: ", response["codeProcess"])
     print("Số lượng code sau khi xử lý không còn lỗ hổng bảo mật: ", response["codeNonVul"])
     print("Số lượng code sau khi xử lý còn lỗ hổng bảo mật: ", response["codeVul"])
